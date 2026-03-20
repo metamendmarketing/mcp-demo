1bad874efa78a5db6acb192ddca0cafe45be5d97
Kevin Osborne <kosborne@metamend.com>
2026-03-20 09:42:48 -0700

    Update basePath and assetPrefix to /mcp/demo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/mcp/demo',
+  assetPrefix: '/mcp/demo',
 };
 
 export default nextConfig;
