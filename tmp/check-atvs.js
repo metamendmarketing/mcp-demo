9e98e0c4e9f30ec0c1e172cca9e435ed90acfc2c
Kevin Osborne <kosborne@metamend.com>
2026-04-07 20:23:32 -0700

    chore: secure tmp directory and prevent API lekage

--- tmp/check-atvs.js
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('prisma/dev.db');
-const summit = db.prepare("SELECT modelName, usageTags FROM Product WHERE modelName = 'Summit'").get();
-console.log('Summit usageTags:', summit.usageTags);
-console.log('Type:', typeof summit.usageTags);
-db.close();
